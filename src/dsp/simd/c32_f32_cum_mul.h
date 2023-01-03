f1e990d57406e8dde16338a28a4a8507343ec877
William Yang <williamyang2017@gmail.com>
2023-01-04 06:27:39 +1100

    Improved c32*f32 AVX2 vectorisation

--- src/dsp/simd/c32_f32_cum_mul.h
+++ src/dsp/simd/c32_f32_cum_mul.h
@@ -74,15 +74,13 @@ std::complex<float> c32_f32_cum_mul_ssse3(const std::complex<float>* x0, const f
 #endif
 
 #if defined(_DSP_AVX2)
-// TODO: Something is seriously wrong with this
-//       Causes L1 cache to be seriously hosed
 static inline
 std::complex<float> c32_f32_cum_mul_avx2(const std::complex<float>* x0, const float* x1, const int N)
 {
     auto y = std::complex<float>(0,0);
 
-    // 256bits = 32bytes = 8*4bytes
-    constexpr int K = 8;
+    // 256bits = 32bytes = 4*8bytes
+    constexpr int K = 4;
     const int M = N/K;
 
     // [3 2 1 0] -> [3 3 2 2]
@@ -93,42 +91,32 @@ std::complex<float> c32_f32_cum_mul_avx2(const std::complex<float>* x0, const fl
     for (int i = 0; i < M; i++) {
         // [c0 c1 c2 c3]
         __m256 a0 = _mm256_load_ps(reinterpret_cast<const float*>(&x0[i*K]));
-        // [c4 c5 c6 c7]
-        __m256 a1 = _mm256_load_ps(reinterpret_cast<const float*>(&x0[i*K + K/2]));
 
-        // [a0 a1 a2 a3 a4 a5 a6 a7]
-        cpx256_t b0;
-        b0.ps = _mm256_load_ps(&x1[i*K]);
+        // [a0 a1 a2 a3]
+        __m128 b0 = _mm_load_ps(&x1[i*K]);
+        // [a2 a2 a3 a3]
+        __m128 b1 = _mm_permute_ps(b0, PERMUTE_LOWER);
+        // [a0 a0 a1 a1]
+        b0 = _mm_permute_ps(b0, PERMUTE_UPPER);
 
         // [a0 a0 a1 a1 a2 a2 a3 a3]
-        cpx256_t b1;
-        b1.m128[0] = _mm_permute_ps(b0.m128[0], PERMUTE_LOWER);
-        b1.m128[1] = _mm_permute_ps(b0.m128[0], PERMUTE_UPPER);
-        // [a4 a4 a5 a5 a6 a6 a7 a7]
-        cpx256_t b2;
-        b2.m128[0] = _mm_permute_ps(b0.m128[1], PERMUTE_LOWER);
-        b2.m128[1] = _mm_permute_ps(b0.m128[1], PERMUTE_UPPER);
+        __m256 a1 = _mm256_set_m128(b0, b1);
 
         // multiply vector
-        cpx256_t c0, c1;
-        c0.ps = _mm256_mul_ps(a0, b1.ps);
-        c1.ps = _mm256_mul_ps(a1, b2.ps);
+        cpx256_t c0;
+        c0.ps = _mm256_mul_ps(a0, a1);
 
         // Perform vectorised cumulative sum
         // Shift half of vector and add. Repeat until we get the final sum
-        cpx256_t d0;
-        // [c0+c4 c1+c5 c2+c6 c3+c7]
-        d0.ps = _mm256_add_ps(c0.ps, c1.ps);
+        cpx128_t d0, d1;
+        // [c0+c2 c1+c3]
+        d0.ps = _mm_add_ps(c0.m128[0], c0.m128[1]);
+        // [c1+c3 0]
+        d1.i = _mm_srli_si128(d0.i, 1*sizeof(std::complex<float>));
+        // [c0+c1+c2+c3 0]
+        d0.ps = _mm_add_ps(d0.ps, d1.ps);
 
-        cpx128_t e0, e1;
-        // [c0+c2+c4+c6 c1+c3+c5+c7]
-        e0.ps = _mm_add_ps(d0.m128[0], d0.m128[1]);
-        // [c1+c3+c5+c7 0]
-        e1.i = _mm_srli_si128(e0.i, 1*sizeof(std::complex<float>));
-        // [c0+c1+c2+c3+c4+c5+c7 0]
-        e0.ps = _mm_add_ps(e0.ps, e1.ps);
-
-        y += e0.c32[0];
+        y += d0.c32[0];
     }
 
     const int N_vector = M*K;
@@ -143,8 +131,7 @@ inline static
 std::complex<float> c32_f32_cum_mul_auto(const std::complex<float>* x0, const float* x1, const int N) {
     #if defined(_DSP_AVX2)
     // return c32_f32_cum_mul_avx2(x0, x1, N);
-    // NOTE: Intel Vtuner shows that this causes the L1 cache to get hosed
-    //       Loads blocked by store forwarding
+    // FIXME: AVX2 performs worse than SSE3 for unknown reasons
     return c32_f32_cum_mul_ssse3(x0, x1, N);
     #elif defined(_DSP_SSSE3)
     return c32_f32_cum_mul_ssse3(x0, x1, N);
